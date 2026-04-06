d76599a4c2170dfb1b6afc246aed56d6913855aa
Robin De Wolf <rdewolf@telenet.be>
2026-04-06 04:50:47 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://fenroar.games',
   vite: {
     plugins: [tailwindcss()]
   }
